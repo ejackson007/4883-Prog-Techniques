438d3c99e34b3f71c0f0fc33225d498d4dedec7f
Evan Jackson <evanojackson@gmail.com>
2020-09-06 16:27:23 -0500

    python working

--- Assignments/P02/514/main.py
+++ Assignments/P02/514/main.py
@@ -9,34 +9,37 @@ valid = True
 car_size = int(input())
 
 while car_size != 0:
-    line.append(list(range(1,car_size+1))) # fill list of cars
+    line = [i for i in range(1, car_size+1)]  # fill list of cars
+    # this list will be modified, therefore kept as a second variable
     cars = copy.deepcopy(line)
     expected_out = [int(i) for i in input().split()]
-    while expected_out[0] != 0: # go to next block
+    while expected_out[0] != 0:  # go to next block
         for car in expected_out:
             while not found:
-                if(car == cars[0]): # next in line
+                if(len(cars) != 0 and car == cars[0]):  # next in line
                     cars.pop(0)
                     found = True
-                elif(len(stack) != 0 and car == stack[-1]): # top of stack
+                elif(len(stack) != 0 and car == stack[-1]):  # top of stack
                     stack.pop()
                     found = True
-                elif(len(cars) == 0 and car != stack[-1]): # no more cars in line, and top of stack isnt it
+                # no more cars in line, and top of stack isnt it
+                elif(len(cars) == 0 and car != stack[-1]):
                     valid = False
                     break
-                else: # put car in line
+                else:  # put car in line
                     stack.append(cars.pop(0))
-            found = False
+            if(valid):
+                found = False
+            else:
+                break
         if(valid):
             print("Yes")
         else:
             print("No")
             valid = True
-            cars = copy.deepcopy(line)
             stack.clear()
         expected_out.clear()
+        cars = copy.deepcopy(line)
         expected_out = [int(i) for i in input().split()]
     car_size = int(input())
-        
-
-
+    print()
