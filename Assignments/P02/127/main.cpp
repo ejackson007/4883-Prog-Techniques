4d0821f28264450dc43615832ce10002a2bbda05
Evan Jackson <evanojackson@gmail.com>
2020-09-09 21:09:35 -0500

    started code

---
+++ Assignments/P02/127/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    vector<vector<string>> deck(52);
+    cout << deck.size();
+}
