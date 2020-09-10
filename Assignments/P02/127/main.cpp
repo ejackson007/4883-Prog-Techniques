4ac38c8669134604d52d3df4bcafdc4f5aeac6e3
Evan Jackson <evanojackson@gmail.com>
2020-09-09 22:17:19 -0500

    extra output

--- Assignments/P02/127/main.cpp
+++ Assignments/P02/127/main.cpp
@@ -10,27 +10,32 @@ int main()
 {
     vector<vector<string> > deck(52);
     string card;
+    bool run = true;
     //cin first half of cards
     for(int i = 0; i < 26; i++){
         cin >> card;
         deck[i].push_back(card);
+        if(card == "#"){
+            run = false;
+            break;
+        }
     }
     //while not eof
-    while(deck[0][0] != "#"){
+    while(run){
         //cin second half of cards
-        for(int i = 0; i < 26; i++){
+        for(int i = 26; i < 52; i++){
             cin >> card;
             deck[i].push_back(card);
         }
         //for cards in deck
-        for(int i = 0; i < 52; i++){
+        for(int i = 1; i < deck.size(); i++){
             //prioritize left most move
             if(i > 2 && (deck[i].back().at(0) == deck[i-3].back().at(0) ||
             deck[i].back().at(1) == deck[i-3].back().at(1))){
                 deck[i-3].push_back(deck[i].back());
                 deck[i].pop_back();
                 if(deck[i].empty())
-                    deck.erase(deck.begin() + 1, deck.begin() + (i+1));
+                    deck.erase(deck.begin() + i, deck.begin() + (i+1));
                 i -= 4;
             }
             if(i > 0 && (deck[i].back().at(0) == deck[i-1].back().at(0) ||
@@ -38,17 +43,28 @@ int main()
                 deck[i-1].push_back(deck[i].back());
                 deck[i].pop_back();
                 if(deck[i].empty())
-                    deck.erase(deck.begin() + 1, deck.begin() + (i+1));
+                    deck.erase(deck.begin() + i, deck.begin() + (i+1));
                 i -= 2;
             }
         }
         if(deck.size() == 1)
-            cout << "1 pile remaining: 52";
+            cout << "1 pile remaining: 52\n";
         else{
-            cout << deck.size() << " piles remaining: ";
+            cout << deck.size() << " piles remaining:";
             for(int i = 0; i < deck.size(); i++)
-                cout << deck[i].size() << " ";
+                cout << " " << deck[i].size();
             cout << endl;
+        }
+        //prepare for next time
+        deck.clear();
+        deck.resize(52);
+        for(int i = 0; i < 26; i++){
+            cin >> card;
+            deck[i].push_back(card);
+            if(card == "#"){
+                run = false;
+                break;
+            }
         }
     }
     return 0;
