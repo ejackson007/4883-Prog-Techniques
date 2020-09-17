0736d5aefb6fdedb6fad75b0ed039a4315e81dab
Evan Jackson <evanojackson@gmail.com>
2020-09-16 20:56:55 -0500

    segmentation fault

--- Assignments/P03/540/main.cpp
+++ Assignments/P03/540/main.cpp
@@ -1,5 +1,12 @@
+//Each team will have its own sub line. 
+
+//How to check if a value is in a team? Map implementation will probably be the most efficcient and easiest.
+//Value : Team format. How do we know the index of the team line though? A class?
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
 
 #define endl '\n'
 
@@ -8,33 +15,48 @@ using namespace std;
 int main(){
     int t, team_size, member, s = 1;
     string command;
-    vector<vector<int> > q;
+    //This will control the order. Instead of reordering the the entire line, a sperate
+    //list will keep track of what teams have which order. 
+    vector<queue<int> > q;
+    unordered_set<int> order;
+
+    unordered_map<int,int> teams;
     cin >> t;
     while(t != 0){
         //create line size
         q.resize(t);
         cout << "Scenario #" << s << endl;
-        //create teams in team members.
-        //this is important as a priority queue sorts into teams, it does not 
-        //take into account groups that are already there
+        //Creates a look up table so we can identify what teams
+        //each number is on. unordered_map makes this O(1)
         for(int i = 0; i < t; i++){
             cin >> team_size;
             for(int j = 0; j < team_size; j++){
                 cin >> member;
+                teams[member] = i;
             }
         }
         //being reading in commands
         cin >> command;
         while(command != "STOP"){
+            //How do i know which queue to put it into?
             if(command == "ENQUEUE"){
-                //do something
+                cin >> member;
+                //add value to unordered set. since unordered set is unique values just push team value
+                order.insert(teams[member]);
+                //push member onto respective team stack
+                q[teams[member]].push(member);
             }
             else{
-                //do something
+                cout << q[*order.begin()].front() << endl;
+                q[*order.begin()].pop();
+                //if the line is empty, it looses its place in line
+                if(q[*order.begin()].empty()){
+                    order.erase(order.begin());
+                }
             }
             cin >> command;
         }
-        q.empty();
+        q.clear();
         s += 1;
         cin >> t;
         cout << endl;
