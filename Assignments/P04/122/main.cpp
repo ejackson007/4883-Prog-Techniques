f5d85c083dd387452841607c3c7a6072d24460bf
Evan Jackson <evan@Evans-Air.lan>
2020-09-19 20:08:10 -0500

    segmentation error

--- Assignments/P04/122/main.cpp
+++ Assignments/P04/122/main.cpp
@@ -2,13 +2,15 @@
 #include <string>
 #include <vector>
 
+#define endl '\n'
+
 using namespace std;
 
 int main(){
     //create tree with maximum amount of nodes, empty
-    int i = 1;
+    int i = 1, leaves = 0, count = 0;
     vector<string> tree;
-    string node, temp, value;
+    string node, value;
     size_t pos;
     //cin first node. If nothing it is end of file
     while(cin >> node){
@@ -16,6 +18,7 @@ int main(){
         tree.resize(257);
         //() ends trees
         while(node != "()"){
+            leaves++;
             //remove ()
             node.erase(node.begin()); 
             node.pop_back();
@@ -34,7 +37,22 @@ int main(){
             i = 1; //reset iterator
             cin >> node;
         }
-        //end of tree. 
+        //end of tree. and print
+        if(tree[1].length() != 0){
+            for(int j = 1; count < leaves; j++){
+                if(tree[j].length() != 0){
+                    cout << tree[j] << " ";
+                    count++;
+                }
+            }
+            cout << endl;
+        }
+        else{
+            cout << "not complete\n";
+        }
+        //reset values
+        leaves = 1;
+        count = 0;
+        tree.clear();
     }
-
 }
