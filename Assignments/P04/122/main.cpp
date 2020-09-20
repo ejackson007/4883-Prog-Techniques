fdfd4f2a56c3983b22b0515f76363c303bb250c3
Evan Jackson <evanojackson@gmail.com>
2020-09-19 22:15:25 -0500

    not sure why its not working. Lots of inputs

--- Assignments/P04/122/main.cpp
+++ Assignments/P04/122/main.cpp
@@ -1,6 +1,12 @@
+//currently failing because of flawed check. It does check to see if it is legitemately on the next level
+//however this does not account to see if the tree has a possible parent. instead of checking for level
+//there needs to be a check if its parent exists. This is possible by determining if its a left or right
+//child, dividing by 2, and then searching for the presence of that value
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm> //sort
+#include <set>
 
 #define endl '\n'
 
@@ -8,16 +14,16 @@ using namespace std;
 
 int main(){
     //create tree with maximum amount of nodes, empty
-    int i = 1, leaves = 0, count = 0;
-    vector<string> tree;
+    int i = 1, leaves = 0, count = 0, prev = 0;
+    vector<pair <int, int> > tree;
+    set <int> positions;
     string node, value;
     size_t pos;
+    bool valid = true; //node can exist
     //cin first node. If nothing it is end of file
     while(cin >> node){
-        //set tree size
-        tree.resize(257);
         //() ends trees
-        while(node != "()"){
+        while(node != "()" && valid){
             leaves++;
             //remove ()
             node.erase(node.begin()); 
@@ -33,26 +39,55 @@ int main(){
                 else
                     i = i*2 + 1;
             }
-            tree[i] = value;
+            //check if its a duplicate
+            if(positions.find(i) != positions.end()){
+                valid = false;
+            }
+            tree.push_back(make_pair(stoi(value), i));
+            positions.insert(i);
             i = 1; //reset iterator
             cin >> node;
         }
-        //end of tree. and print
-        if(tree[1].length() != 0){
-            for(int j = 1; count < leaves; j++){
-                if(tree[j].length() != 0){
-                    cout << tree[j] << " ";
-                    count++;
+        //end of tree. sort by value and print
+
+        //sort vector in ascending order by second value in pair
+        //https://stackoverflow.com/questions/279854/how-do-i-sort-a-vector-of-pairs-based-on-the-second-element-of-the-pair
+        sort(tree.begin(), tree.end(), [](const pair<int,int> &left, const pair<int,int> &right) {
+            return left.second < right.second;});
+
+        //check if first value is a root. 
+        if(valid && tree[0].second == 1){
+            for (pair<int,int> &child : tree){
+                //checks to see if the current leaf is possible, i.e same line or next one
+                //There may be a way to print and do this at the same time.
+                //only way to "rewrite" current line is /r which just moves cursor and therefore 
+                //does not delete the whole row. Worst case (valid) you have to go through vector twice
+                if(child.second != 1 && positions.find(child.second / 2) == positions.end()){
+                    valid = false;
+                    break;
                 }
+                prev = child.second;
             }
-            cout << endl;
         }
         else{
-            cout << "not complete\n";
+            valid = false;
         }
+
+        if(valid){
+            for(int j = 0; j < tree.size() - 1; j++){
+                cout << tree[j].first << " ";
+            }
+            cout << tree.back().first;
+        }
+        else
+            cout << "not complete";
+        cout << endl;
         //reset values
         leaves = 1;
         count = 0;
         tree.clear();
+        positions.clear();
+        valid = true;
+        prev = 0;
     }
 }
