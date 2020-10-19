547584740a9dedecb7960b4a094bc56f1c97d839
Evan Jackson <evanojackson@gmail.com>
2020-10-18 22:50:02 -0500

    float error

--- Assignments/P06/10263/main.cpp
+++ Assignments/P06/10263/main.cpp
@@ -1,23 +1,61 @@
 //Railway
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <cmath>
+
+#define point pair<int,int>
 
 using namespace std;
 
+pair<double, double> closestPoint(point a, point b, point p){
+    point ap = make_pair(p.first - a.first, p.second - a.second);
+    point ab = make_pair(b.first - a.first, b.second - a.second);
+
+    int abSquare = pow(ab.first, 2) + pow(ab.second, 2);
+    int dotProd = ap.first * ab.first + ap.second * ab.second;
+
+    double t = dotProd / abSquare;
+    return(make_pair(a.first + ab.first*t, a.second + ab.second*t));
+}
+
+double distance(point a, point b){
+    //pythagorean
+    return sqrt(pow(b.first - a.first, 2) + pow(b.second - a.second, 2));
+}
+
 int main(){
-    pair<int, int> M, temp;
-    int N;
+    ifstream input;
+    input.open("input");
+
+
+    point M, temp;
+    pair<double, double> out;
+    int N, closest, dis;
     vector<pair<int, int> > segments;
-    while(cin >> M.first){
-        cin >> M.second;
-        cin >> N;
+    while(input >> M.first){
+        input >> M.second;
+        input >> N;
         for(int i = 0; i < N + 1; i++){
-            cin >> temp.first >> temp.second;
+            input >> temp.first >> temp.second;
             segments.push_back(temp);
         }
-        for(auto a : segments){
-            printf("(%i,%i)\n", a.first, a.second);
+        // printf("Origin: (%i,%i)\n", M.first, M.second);
+        // for(auto a : segments){
+        //     printf("(%i,%i)\n", a.first, a.second);
+        // }
+        // cout << endl;
+
+        //find closest point
+        closest = INT_MAX;
+        for(int i = 0; i < segments.size(); i++){
+            dis = distance(segments[i], M);
+            if(dis < closest){
+                closest = dis;
+            }
         }
-        cout << endl;
+        out = closestPoint(segments[closest*2], segments[closest*2 + 1], M);
+        cout << out.first << endl << out.second << endl;
+        segments.clear();
     }
 }
